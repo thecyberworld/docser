b257384e42f2a42aa2bf739d20f6ac90def0e9d4
Yash Singh Chauhan <yashsinghchauhanformal@gmail.com>
2023-09-04 08:38:49 +0530

    feat: added toml parsing logic

--- internal/patterns/patterns.go
+++ internal/patterns/patterns.go
@@ -2,8 +2,9 @@ package patterns
 
 import (
 	"bufio"
-
+	"github.com/BurntSushi/toml"
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"regexp"
 )
 
 // MatchResult represents the result of a regex match
@@ -12,6 +13,11 @@ type MatchResult struct {
 	LineNumber  int
 	MatchString string
 	Pattern     string
+}
+
+// Config defines the structure of the TOML config file
+type Config struct {
+	Patterns map[string]string `toml:"patterns"`
 }
 
 // ProcessTextFileContentsWithRegex reads and processes the contents of a text-based file using regex patterns
@@ -27,11 +33,23 @@ func ProcessTextFileContentsWithRegex(file *object.File, configFile string) ([]M
 	scanner := bufio.NewScanner(fileReader)
 	lineNumber := 0
 
+	// Load patterns from regex.go
+	allPatterns := append([]DefinePatternInfo{}, RegexPatterns...)
+
+	// Load additional patterns from the config file
+	if configFile != "" {
+		configPatterns, err := loadPatternsFromConfigFile(configFile)
+		if err != nil {
+			return nil, err
+		}
+		allPatterns = append(allPatterns, configPatterns...)
+	}
+
 	for scanner.Scan() {
 		line := scanner.Text()
 		lineNumber++
 
-		for _, patternInfo := range RegexPatterns {
+		for _, patternInfo := range allPatterns {
 			regex := patternInfo.Pattern
 			if regex.MatchString(line) {
 				submatches := regex.FindStringSubmatch(line)
@@ -53,4 +71,33 @@ func ProcessTextFileContentsWithRegex(file *object.File, configFile string) ([]M
 	}
 
 	return matchResults, nil
+}
+
+func loadPatternsFromConfigFile(configFile string) ([]DefinePatternInfo, error) {
+	// Read and parse the TOML config file
+	var config Config
+	_, err := toml.DecodeFile(configFile, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	var configPatterns []DefinePatternInfo
+
+	// Compile the regex patterns from the config file
+	for _, patternStr := range config.Patterns {
+		regex, err := regexp.Compile(patternStr)
+		if err != nil {
+			return nil, err
+		}
+
+		// Create a DefinePatternInfo and add it to the configPatterns slice
+		configPattern := DefinePatternInfo{
+			Pattern:     regex,
+			Description: "Config Pattern", // You can set a default description here
+		}
+
+		configPatterns = append(configPatterns, configPattern)
+	}
+
+	return configPatterns, nil
 }
